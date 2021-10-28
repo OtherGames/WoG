7c940b016e1ce06693de386b7bb8518a586cff16
_Woger_ <tischenko@ncv.global>
2021-10-28 03:58:17 +0300

    Я таки сделал ебучий двухслойный перспетрон

--- WoG/Assets/Perceptron.cs
+++ WoG/Assets/Perceptron.cs
@@ -15,6 +15,14 @@ public class Perceptron : MonoBehaviour
 
     [SerializeField]
     private int p = 8;
+    [SerializeField]
+    private int countHiddenNeuron = 5;
+    [SerializeField]
+    float ebala = 0.37f;
+    [SerializeField]
+    float min = -0.5f;
+    [SerializeField]
+    float max = 0.5f;
 
     List<float> etalons = new List<float>();
     [SerializeField]
@@ -23,28 +31,24 @@ public class Perceptron : MonoBehaviour
     float[] yValues;
     float threshold;
 
+    float[,] weightsHidden;
+    float[] thresoldsHidden;
+    float[] weightedSums;
+    float[] outputsHidden;
+    
     [SerializeField]
-    List<float> weightsHiddenLayer_1 = new List<float>();
-    [SerializeField]
-    List<float> weightsHiddenLayer_2 = new List<float>();
-    [SerializeField]
-    List<float> weightsHiddenLayer_3 = new List<float>();
-    [SerializeField]
-    List<float> weightsHiddenLayer_4 = new List<float>();
-    [SerializeField]
-    List<float> weightsHiddenLayer_5 = new List<float>();
-    [SerializeField]
-    List<float> weightsOutputLayer = new List<float>();
-    float thresoldHiddenLayer_1;
-    float thresoldHiddenLayer_2;
-    float thresoldHiddenLayer_3;
-    float thresoldHiddenLayer_4;
-    float thresoldHiddenLayer_5;
+    float[] weightsOutputLayer;
     float thresoldOutputLayer;
-    List<float> outputsHiddenLayer = new List<float>();
+
 
     private IEnumerator Start()
     {
+        weightsHidden = new float[countHiddenNeuron, p];
+        thresoldsHidden = new float[countHiddenNeuron];
+        outputsHidden = new float[countHiddenNeuron];
+        weightsOutputLayer = new float[countHiddenNeuron];
+        weightedSums = new float[countHiddenNeuron];
+
         var capacity = rowLength - p;
         yValues = new float[capacity];
 
@@ -52,197 +56,202 @@ public class Perceptron : MonoBehaviour
         pos.x = lineRenderer.transform.position.x + p;
         predictLine.transform.position = pos;
 
-        for (int i = 0; i < rowLength; i++)
-        {
-            yield return null;
-
-            float x = i;
-            float y = 1.8f * Mathf.Sin(1.3f * i);// + 0.5f;
-            lineRenderer.positionCount = i + 1;
-            lineRenderer.SetPosition(i, new Vector2(x, y));
-
-            etalons.Add(y);
-        }
-
-        //etalons.Add(-1.153f);
-        //etalons.Add(-1.153f);
-        //etalons.Add(-1.053f);
-        //etalons.Add(-1.153f);
-        //etalons.Add(-0.153f);
-        //etalons.Add(-0.153f);
-        //etalons.Add(1.123f);
-        //etalons.Add(-1.153f);
-        //etalons.Add(-1.053f);
-        //etalons.Add(-1.153f);
-        //etalons.Add(1.153f);
-        //etalons.Add(0.153f);
-        //etalons.Add(-1.153f);
-        //etalons.Add(-1.153f);
-        //etalons.Add(-0.153f);
-        //etalons.Add(0.153f);
-        //etalons.Add(-0.153f);
-        //etalons.Add(-1.113f);
-        //etalons.Add(-0.153f);
-        //etalons.Add(1.153f);
-        //etalons.Add(-0.153f);
-        //etalons.Add(-0.133f);
-        //etalons.Add(-0.153f);
-        //etalons.Add(-0.143f);
-        //etalons.Add(1.153f);
-        //etalons.Add(-0.153f);
-        //etalons.Add(-0.153f);
-        //etalons.Add(-0.583f);
-        //etalons.Add(-1.153f);
-        //etalons.Add(-0.153f);
-
         //for (int i = 0; i < rowLength; i++)
         //{
         //    yield return null;
+
+        //    float x = i;
+        //    //float y = 1.8f * Mathf.Sin(1.3f * i);// + 0.5f;
+        //    float y = .8f * Mathf.Sin(1.3f * i);// + 0.5f;
+        //    //y = Mathf.Clamp01(y);
         //    lineRenderer.positionCount = i + 1;
-        //    lineRenderer.SetPosition(i, new Vector2(i, etalons[i]));
+        //    lineRenderer.SetPosition(i, new Vector2(x, y));
+
+        //    etalons.Add(y);
         //}
 
-        float min = -0.8f;
-        float max = 0.8f;
+        etalons.Add(-1.153f);
+        etalons.Add(-1.153f);
+        etalons.Add(-1.053f);
+        etalons.Add(-1.153f);
+        etalons.Add(-0.153f);
+        etalons.Add(-0.153f);
+        etalons.Add(1.123f);
+        etalons.Add(-1.153f);
+        etalons.Add(-1.053f);
+        etalons.Add(-1.153f);
+        etalons.Add(1.153f);
+        etalons.Add(0.153f);
+        etalons.Add(-1.153f);
+        etalons.Add(-1.153f);
+        etalons.Add(-0.153f);
+        etalons.Add(0.153f);
+        etalons.Add(-0.153f);
+        etalons.Add(-1.113f);
+        etalons.Add(-0.153f);
+        etalons.Add(1.153f);
+        etalons.Add(-0.153f);
+        etalons.Add(-0.133f);
+        etalons.Add(-0.153f);
+        etalons.Add(-0.143f);
+        etalons.Add(1.153f);
+        etalons.Add(-0.153f);
+        etalons.Add(-0.153f);
+        etalons.Add(-0.583f);
+        etalons.Add(-1.153f);
+        etalons.Add(-0.153f);
+        etalons.Add(-0.583f);
+        etalons.Add(-0.353f);
+        etalons.Add(-0.153f);
+
+        for (int i = 0; i < rowLength; i++)
+        {
+            yield return null;
+            lineRenderer.positionCount = i + 1;
+            lineRenderer.SetPosition(i, new Vector2(i, etalons[i]));
+        }
+
+
+
         // инициализация весов
-        for (int i = 0; i < p; i++)
-        {
-            weightsHiddenLayer_1.Add(Random.Range(min, max));
-            weightsHiddenLayer_2.Add(Random.Range(min, max));
-            weightsHiddenLayer_3.Add(Random.Range(min, max));
-            weightsHiddenLayer_4.Add(Random.Range(min, max));
-            weightsHiddenLayer_5.Add(Random.Range(min, max));
-        }
-
-        for (int i = 0; i < 5; i++)
-        {
-            weightsOutputLayer.Add(Random.Range(min, max));
-        }
-
-        thresoldHiddenLayer_1 = Random.Range(min, max);
-        thresoldHiddenLayer_2 = Random.Range(min, max);
-        thresoldHiddenLayer_3 = Random.Range(min, max);
-        thresoldHiddenLayer_4 = Random.Range(min, max);
-        thresoldHiddenLayer_5 = Random.Range(min, max);
         thresoldOutputLayer = Random.Range(min, max);
 
+        for (int i = 0; i < countHiddenNeuron; i++)
+        {
+            for (int j = 0; j < p; j++)
+            {
+                weightsHidden[i, j] = Random.Range(min, max);
+            }
 
-        for (int iSample = 0; iSample < rowLength - p; iSample++)
+            thresoldsHidden[i] = Random.Range(min, max);
+            weightsOutputLayer[i] = Random.Range(min, max);
+        }
+
+
+        // Подaча входных образов
+        for (int pisdat = 0; pisdat < 10000; pisdat++)
         {
             yield return null;
-            // Подaча входных образов
-            float weightedSumHiddelLayer = 0;
-            // Вычисление выходных значений 1-го нейрона скрытого слоя
-            for (int i = 0; i < p; i++)
+            print($"Итерация я ебу...{pisdat}");
+
+            for (int iSample = 0; iSample < rowLength - p; iSample++)
             {
-                float x = etalons[i + iSample];
-                weightedSumHiddelLayer += weightsHiddenLayer_1[i] * x;
+
+                for (int i = 0; i < countHiddenNeuron; i++)
+                {
+                    float wSum = 0;
+                    for (int j = 0; j < p; j++)
+                    {
+                        float x = etalons[j + iSample];
+                        wSum += weightsHidden[i, j] * x;
+                    }
+                    wSum -= thresoldsHidden[i];
+                    weightedSums[i] = wSum;
+                    outputsHidden[i] = SigmoidActivation(wSum);
+                }
+
+
+                // Вычисление выходных значений 1-го нейрона скрытого слоя
+                //for (int i = 0; i < p; i++)
+                //{
+                //    float x = etalons[i + iSample];
+                //    weightedSumHiddelLayer += weightsHiddenLayer_1[i] * x;
+                //}
+                //weightedSumHiddelLayer -= thresoldHiddenLayer_1;
+
+                //float output_1 = SigmoidActivation(weightedSumHiddelLayer);
+
+                //if (outputsHiddenLayer.Count < 5)
+                //    outputsHiddenLayer.Add(output_1);
+                //else
+                //    outputsHiddenLayer[0] = output_1;
+
+
+
+
+                //yield return null;
+                // Вычисление выходных значений выходного слоя
+                float weightedSumOutputLayer = 0;
+                for (int i = 0; i < countHiddenNeuron; i++)
+                {
+                    float x = outputsHidden[i];
+                    weightedSumOutputLayer += weightsOutputLayer[i] * x;
+                }
+                weightedSumOutputLayer -= thresoldOutputLayer;
+                //float outputMain = SigmoidActivation(weightedSumOutputLayer);
+                float outputMain = weightedSumOutputLayer;
+
+
+                yValues[iSample] = outputMain;
+                if (predictLine.positionCount < rowLength - p)
+                    predictLine.positionCount = iSample + 1;
+                predictLine.SetPosition(iSample, new Vector3(iSample, outputMain));
+
+
+
+
+                // Изминение порогов и весовых коэффициентов
+                // Ебаный пиздец...
+
+                //Вычисение ошибок для выходного слоя и скрытых слоев
+                float errOutput = outputMain - etalons[iSample + p];
+
+                float[] errHiden = new float[countHiddenNeuron];
+                for (int i = 0; i < countHiddenNeuron; i++)
+                {
+                    errHiden[i] = errOutput * DerivativeSigmoid(weightedSumOutputLayer) * weightsOutputLayer[i];
+                }
+
+                float a = 0.05f;
+
+                // Корректировка весов от скрытого слоя к выходному i -> j
+                //yield return null;
+                for (int i = 0; i < countHiddenNeuron; i++)
+                {
+                    float w = weightsOutputLayer[i] - a * errOutput * DerivativeSigmoid(weightedSumOutputLayer) * outputsHidden[i];
+                    weightsOutputLayer[i] = w;
+                }
+
+                thresoldOutputLayer = thresoldOutputLayer + a * errOutput * DerivativeSigmoid(weightedSumOutputLayer);
+
+                // Корректировка весов от входного слоя к скрытому
+
+                for (int i = 0; i < countHiddenNeuron; i++)
+                {
+                    for (int j = 0; j < p; j++)
+                    {
+                        float w = weightsHidden[i, j];
+                        float x = etalons[j + iSample];
+                        w = w - a * errHiden[i] * DerivativeSigmoid(weightedSums[i]) * x;
+                        weightsHidden[i, j] = w;
+                    }
+                    thresoldsHidden[i] = thresoldsHidden[i] + a * errHiden[i] * DerivativeSigmoid(weightedSums[i]);
+                }
+
             }
-            weightedSumHiddelLayer -= thresoldHiddenLayer_1;
-
-            float output_1 = SigmoidActivation(weightedSumHiddelLayer);
-            
-            if(outputsHiddenLayer.Count < 5)
-                outputsHiddenLayer.Add(output_1);
-            else
-                outputsHiddenLayer[0] = output_1;
-
-            yield return null;
-            // Вычисление выходных значений 2-го нейрона скрытого слоя
-            weightedSumHiddelLayer = 0;
-            for (int i = 0; i < p; i++)
-            {
-                float x = etalons[i + iSample];
-                weightedSumHiddelLayer += weightsHiddenLayer_2[i] * x;
-            }
-            weightedSumHiddelLayer -= thresoldHiddenLayer_2;
-
-            float output_2 = SigmoidActivation(weightedSumHiddelLayer);
-            if (outputsHiddenLayer.Count < 5)
-                outputsHiddenLayer.Add(output_2);
-            else
-                outputsHiddenLayer[1] = output_2;
-
-            yield return null;
-            // Вычисление выходных значений 3-го нейрона скрытого слоя
-            weightedSumHiddelLayer = 0;
-            for (int i = 0; i < p; i++)
-            {
-                float x = etalons[i + iSample];
-                weightedSumHiddelLayer += weightsHiddenLayer_3[i] * x;
-            }
-            weightedSumHiddelLayer -= thresoldHiddenLayer_3;
-
-            float output_3 = SigmoidActivation(weightedSumHiddelLayer);
-            if (outputsHiddenLayer.Count < 5)
-                outputsHiddenLayer.Add(output_3);
-            else
-                outputsHiddenLayer[2] = output_3;
-
-            yield return null;
-            // Вычисление выходных значений 4-го нейрона скрытого слоя
-            weightedSumHiddelLayer = 0;
-            for (int i = 0; i < p; i++)
-            {
-                float x = etalons[i + iSample];
-                weightedSumHiddelLayer += weightsHiddenLayer_4[i] * x;
-            }
-            weightedSumHiddelLayer -= thresoldHiddenLayer_4;
-
-            float output_4 = SigmoidActivation(weightedSumHiddelLayer);
-            if (outputsHiddenLayer.Count < 5)
-                outputsHiddenLayer.Add(output_4);
-            else
-                outputsHiddenLayer[3] = output_4;
-
-            yield return null;
-            // Вычисление выходных значений 5-го нейрона скрытого слоя
-            weightedSumHiddelLayer = 0;
-            for (int i = 0; i < p; i++)
-            {
-                float x = etalons[i + iSample];
-                weightedSumHiddelLayer += weightsHiddenLayer_5[i] * x;
-            }
-            weightedSumHiddelLayer -= thresoldHiddenLayer_5;
-
-            float output_5 = SigmoidActivation(weightedSumHiddelLayer);
-            if (outputsHiddenLayer.Count < 5)
-                outputsHiddenLayer.Add(output_5);
-            else
-                outputsHiddenLayer[4] = output_5;
-
-
-            yield return null;
-            // Вычисление выходных значений выходного слоя
-            float weightedSumOutputLayer = 0;
-            for (int i = 0; i < outputsHiddenLayer.Count; i++)
-            {
-                float x = outputsHiddenLayer[i];
-                weightedSumOutputLayer += weightsOutputLayer[i] * x;
-            }
-            weightedSumOutputLayer -= thresoldOutputLayer;
-            float outputMain = weightedSumOutputLayer;//SigmoidActivation(weightedSumOutputLayer);
-
-
-
-            yValues[iSample] = outputMain;
-            if (predictLine.positionCount < rowLength - p)
-                predictLine.positionCount = iSample + 1;
-            predictLine.SetPosition(iSample, new Vector3(iSample, outputMain));
-
-            // Изминение порогов и весовых коэффициентов
-            // Ебаный пиздец...
-
-
         }
     }
 
     float SigmoidActivation(float weightedSum)
     {
-        var denominator = 1 + Mathf.Pow(Mathf.Epsilon, -weightedSum);
-        var numerator = 1;
-        return numerator / denominator;
-        //return weightedSum;
+        //var denominator = 1 + Mathf.Pow(Mathf.Epsilon, -weightedSum);
+        //var numerator = 1;
+        //return numerator / denominator;
+
+        if (weightedSum > 0)
+            return weightedSum;
+        else
+            return 0.01f;
+        
+    }
+
+    float DerivativeSigmoid(float weightedSum)
+    {
+        //return SigmoidActivation(weightedSum) * (1 - SigmoidActivation(weightedSum));
+        if (weightedSum > 0)
+            return 1;
+        else
+            return 0.01f;
     }
 
     IEnumerator OneLayerPerchick()
